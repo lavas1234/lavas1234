d66d741f537c9181d4f1ca9109cfea6b143fa8f9
lavas1234 <sailavanya.a9@gmail.com>
2018-10-31 18:57:07 +0530

    author : sailavanya.a9@gmail.com description : hai

---
+++ Hello World.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld{
+public void display(int x,int y)
+{
+System.out.println("Sum of the two numbers:"+(x+y));
+}
+public static void main(String[]args)
+{
+HelloWorld helloworld = new HelloWorld();
+helloWorld.display(10,20);
+}
+}
