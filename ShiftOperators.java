4d68a5107bd02ff08dca072a68bb2986427e5a4c
lavas1234 <sailavanya.a9@gmail.com>
2018-11-01 21:58:54 +0530

    author : sailavanya.a9@gmail.com description : pooja

---
+++ ShiftOperators.java
@@ -0,0 +1,16 @@
+public  class ShiftOperators
+{
+public void display()
+{
+System.out.println(12<<2);
+System.out.println(34<<5);
+System.out.println(425<<2);
+System.out.println(-30<<2);
+System.out.println(22<<4);
+}
+public static void main(String[]args)
+{
+ShiftOperators pooja = new ShiftOperators();
+pooja.display();
+}
+}
